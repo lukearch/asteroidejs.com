5a8060d6c85a8fc77389701777efe5ad06ccec43
Lucas Larangeira <lukearch@proton.me>
2024-07-07 10:35:49 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizeCss: true,
+    typedRoutes: true
+  }
+};
 
 export default nextConfig;
